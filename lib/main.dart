87b7626635f495140e949231448b903f028908f0
RakshitWalia <rakshitwalia2002@gmail.com>
2021-08-25 20:54:00 +0530

    new main

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black38,
-        body: Center(
-            child: CircleAvatar(
+        home: Scaffold(
+            backgroundColor: Colors.black38,
+            body: ConstrainedBox(
+                constraints: BoxConstraints.expand(),
                 child: TextButton(
-                  style: TextButton.styleFrom(
-                      shape: CircleBorder(), padding: EdgeInsets.all(30)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      "SOS",
-                      style: TextStyle(
-                        fontSize: 70,
-                      ),
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-                radius: 125,
-                backgroundImage: Image(
-                  image: Image.network(
-                      "https://images.squarespace-cdn.com/content/v1/5ea5ce810e499b6925447843/1615872071916-LLMGNZ48004MRA1ANXT2/SOS_logo_white.png?format=1500w"),
-                ))),
-      ),
-    );
+                    style: TextButton.styleFrom(
+                        shape: CircleBorder(), padding: EdgeInsets.all(50)),
+                    onPressed: () {},
+                    child: Image.network(
+                        'http://www.clker.com/cliparts/n/X/k/A/m/X/glossy-sos-circle-button.svg.med.png')))));
   }
 }
